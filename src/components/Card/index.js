63cace26d1d1b68234172f0837391fe9905b6b00
Elton de Almeida <elton_de_almeida@hotmail.com>
2022-01-19 23:26:54 -0300

    Project completion

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function Card() {
+ return (
+  <div className="card-container">
+   <p>Components:</p>
+   <p>Makes it easier to keep smaller parts running smoothly</p>
+   <p>You can reuse them, i.e., less code to write</p>
+  </div>
+ );
+}
